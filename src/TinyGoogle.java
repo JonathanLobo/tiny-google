06e451c3f3c2d9fedbeb1473a77c3e48fb43c43b
Jonathan Lobo <jpl68@pitt.edu>
2018-12-10 20:49:47 -0500

    added wrapper program and fixed bugs

--- src/TinyGoogle.java
+++ src/TinyGoogle.java
@@ -13,7 +13,9 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 // import org.apache.hadoop.mapred.*;
 // import org.apache.hadoop.util.*;
 
-public class InvertedIndex {
+public class TinyGoogle {
+
+    public static HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
 
     public static class InvertedIndexMapper extends Mapper<LongWritable, Text, Text, Text> {
 
@@ -53,7 +55,6 @@ public class InvertedIndex {
 
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-            HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
             Iterator<Text> vals = values.iterator();
             while (vals.hasNext()) {
                 // value for each line is docId (key = word, value = docId)
@@ -71,7 +72,6 @@ public class InvertedIndex {
                     hashMap.put(docId, currentCount);
                 }
             }
-            // output.collect(key, new Text(hashMap.toString()));
 
             // Set output format
             boolean isFirst = true;
@@ -88,24 +88,133 @@ public class InvertedIndex {
         }
     }
 
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-
+    public static void mapReduceIndex(Path inPath, Path outPath) throws IOException, InterruptedException {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "InvertedIndex");
+        Job job = Job.getInstance(conf, "TinyGoogle");
         job.setJarByClass(TinyGoogle.class);
         job.setMapperClass(InvertedIndexMapper.class);
         job.setCombinerClass(InvertedIndexReducer.class);
         job.setReducerClass(InvertedIndexReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, inPath);
+        FileOutputFormat.setOutputPath(job, outPath);
 
         try {
-            job.waitForCompletion(false);
+            job.waitForCompletion(true);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        FileOutputStream fout = new FileOutputStream("tiny_google.indx");
+        ObjectOutputStream oos = new ObjectOutputStream(fout);
+        oos.writeObject(hashMap);
+        fout.close();
+        oos.close();
+    }
+
+    public static boolean indexExists() {
+        if (new File("tiny_google.indx").isFile()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void buildInvertedIndex(int mode) throws IOException, InterruptedException {
+        System.out.println("____________________________________________________________________");
+        if (mode == 1) {
+            System.out.println("Building new Inverted Index.");
+        } else {
+            System.out.println("Adding new directory to Inverted Index.");
+        }
+        System.out.println("____________________________________________________________________");
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Please enter path of a directory to index:\t");
+        Path inPath = new Path(kb.nextLine());
+        System.out.print("Please enter output path:\t");
+        Path outPath = new Path(kb.nextLine());
+        System.out.println("____________________________________________________________________");
+        if (mode == 1) {
+            System.out.println("\tPlease wait while the inverted index is generated ... ");
+        } else {
+            System.out.println("\tPlease wait while the inverted index is updated ... ");
+        }
+        System.out.println("____________________________________________________________________");
+        try {
+            mapReduceIndex(inPath, outPath);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("____________________________________________________________________");
+        return;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+
+        System.out.println("____________________________________________________________________");
+        System.out.println("\t\tWelcome to TinyGoogle!");
+        System.out.println("____________________________________________________________________");
+        int input;
+        Scanner kb = new Scanner(System.in);
+        if (!indexExists()) {
+            while(true) {
+                System.out.println("No existing inverted index was found.\nWould you like to generate one now?\n\t1. Yes\n\t2. No");
+                input = kb.nextInt();
+                if(input > 2 || input < 1) {
+                    System.out.println("Invalid option. Please try again.\n");
+                } else if (input == 1) {
+                    buildInvertedIndex(0);
+                    break;
+                } else {
+                    System.out.println("Note: No queries will be possible until a directory is indexed.");
+                    System.out.println("____________________________________________________________________");
+                    break;
+                }
+            }
+        }
+        else {
+            while(true) {
+                System.out.println("An index already exists on disk.\nWould you like to use the existing inverted index or build a new one?\n\t1. Use existing\n\t2. Build new");
+                input = kb.nextInt();
+                if(input > 2 || input < 1){
+                    System.out.println("Invalid option. Please try again.\n");
+                } else if (input == 2) {
+                    System.out.println("The existing inverted index will be deleted.");
+                    new File("tiny_google.indx").delete();
+                    buildInvertedIndex(0);
+                    break;
+                } else {
+                    System.out.println("Using the existing inverted index.");
+                    FileInputStream fin = new FileInputStream("tiny_google.indx");
+                    ObjectInputStream ois = new ObjectInputStream(fin);
+                    hashMap = (HashMap<String, Integer>) ois.readObject();
+                    System.out.println("____________________________________________________________________");
+                    break;
+                }
+            }
+        }
+
+        do {
+            System.out.println("Please choose an option:\n\t1. Perform a search query \n\t2. Add another directory to the index\n\t3. Quit");
+            input = kb.nextInt();
+            if(input > 3 || input < 1){
+                System.out.println("Not a valid option.\nPlease try again.");
+                continue;
+            }
+            if (input == 1 && !indexExists()) {
+                System.out.println("No inverted index exists.\nPlease create an inverted index before attempting a query.");
+                System.out.println("____________________________________________________________________");
+            } else if (input == 1) {
+                // query the inverted index
+            } else if (input == 2) {
+                new File("tiny_google.indx").delete();
+                buildInvertedIndex(1);
+            }
+        } while(input != 3);
+        System.out.println("____________________________________________________________________");
+        System.out.println("Goodbye!");
+        System.out.println("____________________________________________________________________");
+
     }
 }
