b49f2679113e6da62ab5c8a1ac64982d1924fa1b
Jonathan Lobo <jpl68@pitt.edu>
2018-12-12 20:38:32 -0500

    update howto

--- tiny-google.sh
+++ tiny-google.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 clear
+mkdir class
 javac -verbose -classpath /opt/hadoop/share/hadoop/common/hadoop-common-2.7.2.jar:/opt/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.2.jar -d class src/*.java -source 1.7 -target 1.7
 jar -cvf TinyGoogle.jar -C class/ .
 hadoop jar TinyGoogle.jar TinyGoogle
